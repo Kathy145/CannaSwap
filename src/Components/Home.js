0f4021585f5ba952533eadd0863fbc7bee2ffd4f
Kathy145 <Kathy.Lambert@LotusDesignsAgency.com>
2018-02-20 15:30:57 -0500

    Corrected repo name.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react'
 class Home extends Component {
   render() {
     return (<div className="page-container">
-      <h1>Wanna Swap, CannaSwap
-        
-      </h1>
+      <h1>Wanna Swap, CannaSwap</h1>
     </div>)
   }
 }
